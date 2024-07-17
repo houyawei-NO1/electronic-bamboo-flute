616b57c7d7df9847f3ed3277bb35079dec6133de
houyawei-NO1 <30817974+houyawei-NO1@users.noreply.github.com>
2024-07-17 18:06:52 +0800

    2407017

--- usr/main.c
+++ usr/main.c
@@ -1,42 +1,65 @@
-/*---------------------------------------------------------------------*/
-/* --- STC MCU Limited ------------------------------------------------*/
-/* --- STC 1T Series MCU Demo Programme -------------------------------*/
-/* --- Mobile: (86)13922805190 ----------------------------------------*/
-/* --- Fax: 86-0513-55012956,55012947,55012969 ------------------------*/
-/* --- Tel: 86-0513-55012928,55012929,55012966 ------------------------*/
-/* --- Web: www.STCAI.com ---------------------------------------------*/
-/* --- Web: www.STCMCUDATA.com  ---------------------------------------*/
-/* --- BBS: www.STCAIMCU.com  -----------------------------------------*/
-/* --- QQ:  800003751 -------------------------------------------------*/
-/* 如果要在程序中使用此代码,请在程序中注明使用了STC的资料及程序            */
-/*---------------------------------------------------------------------*/
-
 //#include	"\..\library\config.h"
 #include	"\..\library\STC32G_GPIO.h"
 #include	"\..\library\STC32G_Delay.h"
 #include	"\..\library\STC32G_UART.h"
+#include	"\..\library\STC32G_ADC.h"
+#include	"\..\library\STC32G_PWM.h"
 #include	"\..\library\STC32G_NVIC.h"//中断
 #include	"\..\library\STC32G_Switch.h"//功能脚切换
 
-/***************	功能说明	****************
+//下载时, 选择时钟 24MHz (可以在配置文件"config.h"中修改).
 
-程序使用P6口来演示跑马灯。
+/*************	本地常量声明	**************/
 
-下载时, 选择时钟 24MHz (可以在配置文件"config.h"中修改).
 
-******************************************/
+/*************	本地变量声明	**************/
+
+PWMx_Duty PWMA_Duty;
+bit PWM1_Flag;
+bit PWM2_Flag;
+bit PWM3_Flag;
+bit PWM4_Flag;
+
+/*************	本地函数声明	**************/
+void	PWM_config(void);
+void	UART_config(void);
+void    GPIO_config(void);
+/*************  外部函数和变量声明 *****************/
+
+
+
+
+
 
 //========================================================================
-//                               本地常量声明	
+// 函数: main
+// 描述: 用户应用程序.
+// 参数: None.
+// 返回: None.
+// 版本: V1.0, 2020-09-23
 //========================================================================
+void main(void)
+{
+	WTST = 0;		//设置程序指令延时参数，赋值为0可将CPU执行指令的速度设置为最快
+	EAXSFR();		//扩展SFR(XFR)访问使能 
+	CKCON = 0;      //提高访问XRAM速度
 
-u8 code ledNum[]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+	GPIO_config();
+	P35 = 0;		
+	UART_config();
+	PWM_config();
+	EA = 1;
 
-//========================================================================
-//                               本地变量声明
-//========================================================================
+	printf("STC32G AD to UART Test Programme!\r\n");	//UART发送一个字符串
+	
+	while(1)
+	{
+		delay_ms(1000);
+		P34 = ~P34;	//输出低驱动
+		printf("STC32G AD to UART Test Programme!\r\n");	//UART发送一个字符串
 
-u8 ledIndex;
+	}
+}
 
 //========================================================================
 // 函数: GPIO_config
@@ -65,39 +88,48 @@ void	UART_config(void)
 	UART1_SW(UART1_SW_P30_P31);		//UART1_SW_P30_P31,UART1_SW_P36_P37,UART1_SW_P16_P17,UART1_SW_P43_P44
 }
 
-//========================================================================
-// 函数: main
-// 描述: 用户应用程序.
-// 参数: None.
-// 返回: None.
-// 版本: V1.0, 2020-09-23
-//========================================================================
-void main(void)
+/***************  PWM初始化函数 *****************/
+void	PWM_config(void)
 {
-	WTST = 0;		//设置程序指令延时参数，赋值为0可将CPU执行指令的速度设置为最快
-	EAXSFR();		//扩展SFR(XFR)访问使能 
-	CKCON = 0;      //提高访问XRAM速度
-
-	GPIO_config();
-	P35 = 0;		
-	UART_config();
-	EA = 1;
-
-	printf("STC32G AD to UART Test Programme!\r\n");	//UART发送一个字符串
+	PWMx_InitDefine		PWMx_InitStructure;
 	
-	while(1)
-	{
-		delay_ms(1000);
-		P34 = ~P34;	//输出低驱动
-		printf("STC32G AD to UART Test Programme!\r\n");	//UART发送一个字符串
-//		ledIndex++;
-//		if(ledIndex > 7)
-//		{
-//			ledIndex = 0;
-//		}
-	}
+	PWMA_Duty.PWM1_Duty = 128;
+	PWMA_Duty.PWM2_Duty = 256;
+	PWMA_Duty.PWM3_Duty = 512;
+	PWMA_Duty.PWM4_Duty = 1024;
+
+	PWMx_InitStructure.PWM_Mode    =	CCMRn_PWM_MODE1;	//模式,		CCMRn_FREEZE,CCMRn_MATCH_VALID,CCMRn_MATCH_INVALID,CCMRn_ROLLOVER,CCMRn_FORCE_INVALID,CCMRn_FORCE_VALID,CCMRn_PWM_MODE1,CCMRn_PWM_MODE2
+	PWMx_InitStructure.PWM_Duty    = PWMA_Duty.PWM1_Duty;	//PWM占空比时间, 0~Period
+	PWMx_InitStructure.PWM_EnoSelect   = ENO1P | ENO1N;	//输出通道选择,	ENO1P,ENO1N,ENO2P,ENO2N,ENO3P,ENO3N,ENO4P,ENO4N / ENO5P,ENO6P,ENO7P,ENO8P
+	PWM_Configuration(PWM1, &PWMx_InitStructure);				//初始化PWM1
+
+	PWMx_InitStructure.PWM_Mode    =	CCMRn_PWM_MODE1;	//模式,		CCMRn_FREEZE,CCMRn_MATCH_VALID,CCMRn_MATCH_INVALID,CCMRn_ROLLOVER,CCMRn_FORCE_INVALID,CCMRn_FORCE_VALID,CCMRn_PWM_MODE1,CCMRn_PWM_MODE2
+	PWMx_InitStructure.PWM_Duty    = PWMA_Duty.PWM2_Duty;	//PWM占空比时间, 0~Period
+	PWMx_InitStructure.PWM_EnoSelect   = ENO2P | ENO2N;	//输出通道选择,	ENO1P,ENO1N,ENO2P,ENO2N,ENO3P,ENO3N,ENO4P,ENO4N / ENO5P,ENO6P,ENO7P,ENO8P
+	PWM_Configuration(PWM2, &PWMx_InitStructure);				//初始化PWM2
+
+	PWMx_InitStructure.PWM_Mode    =	CCMRn_PWM_MODE1;	//模式,		CCMRn_FREEZE,CCMRn_MATCH_VALID,CCMRn_MATCH_INVALID,CCMRn_ROLLOVER,CCMRn_FORCE_INVALID,CCMRn_FORCE_VALID,CCMRn_PWM_MODE1,CCMRn_PWM_MODE2
+	PWMx_InitStructure.PWM_Duty    = PWMA_Duty.PWM3_Duty;	//PWM占空比时间, 0~Period
+	PWMx_InitStructure.PWM_EnoSelect   = ENO3P | ENO3N;	//输出通道选择,	ENO1P,ENO1N,ENO2P,ENO2N,ENO3P,ENO3N,ENO4P,ENO4N / ENO5P,ENO6P,ENO7P,ENO8P
+	PWM_Configuration(PWM3, &PWMx_InitStructure);				//初始化PWM3
+
+	PWMx_InitStructure.PWM_Mode    =	CCMRn_PWM_MODE1;	//模式,		CCMRn_FREEZE,CCMRn_MATCH_VALID,CCMRn_MATCH_INVALID,CCMRn_ROLLOVER,CCMRn_FORCE_INVALID,CCMRn_FORCE_VALID,CCMRn_PWM_MODE1,CCMRn_PWM_MODE2
+	PWMx_InitStructure.PWM_Duty    = PWMA_Duty.PWM4_Duty;	//PWM占空比时间, 0~Period
+	PWMx_InitStructure.PWM_EnoSelect   = ENO4P | ENO4N;	//输出通道选择,	ENO1P,ENO1N,ENO2P,ENO2N,ENO3P,ENO3N,ENO4P,ENO4N / ENO5P,ENO6P,ENO7P,ENO8P
+	PWM_Configuration(PWM4, &PWMx_InitStructure);				//初始化PWM4
+
+	PWMx_InitStructure.PWM_Period   = 2047;							//周期时间,   0~65535
+	PWMx_InitStructure.PWM_DeadTime = 0;								//死区发生器设置, 0~255
+	PWMx_InitStructure.PWM_MainOutEnable= ENABLE;				//主输出使能, ENABLE,DISABLE
+	PWMx_InitStructure.PWM_CEN_Enable   = ENABLE;				//使能计数器, ENABLE,DISABLE
+	PWM_Configuration(PWMA, &PWMx_InitStructure);				//初始化PWM通用寄存器,  PWMA,PWMB
+
+	PWM1_USE_P60P61();
+	PWM2_USE_P62P63();
+	PWM3_USE_P64P65();
+	PWM4_USE_P66P67();
+
+	NVIC_PWM_Init(PWMA,DISABLE,Priority_0);
 }
-
-
 
 
